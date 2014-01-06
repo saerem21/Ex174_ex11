e29ab0150a458c68f27d4c7c3c283c985e361bde
saerem21 <nemessisdds>
2014-01-06 17:12:50 +0900

    테이블뷰 악세사리 테스트 입니다.
    174page

--- Ex174_ex11/ViewController.m
+++ Ex174_ex11/ViewController.m
@@ -5,19 +5,52 @@
 //  Created by SDT-1 on 2014. 1. 6..
 //  Copyright (c) 2014년 SDT-1. All rights reserved.
 //
-
+//
 #import "ViewController.h"
-
-@interface ViewController ()
+#define CELL_ID @"CELL_ID"
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+{
+    NSArray *data;
+}
 
 @end
 
 @implementation ViewController
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"select Row %d",indexPath.row);
+}
+
+- (void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"악세사리 버튼 눌림 %d",indexPath.row );
+}
+
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID];
+    if(0 == indexPath.row)cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    else if(1 == indexPath.row)cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+    else if(2 == indexPath.row)cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+
+    cell.textLabel.text = data[indexPath.row];
+    
+    
+    cell.textLabel.text = data[indexPath.row];
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [data count];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    data = @[@"a",@"b",@"c",@"d",@"e",@"f",@"g",@"h",@"i"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,3 +60,4 @@
 }
 
 @end
+
